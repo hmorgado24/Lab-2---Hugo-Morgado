db9c24d6554e8f6fd000b37f1470b71e867710ef
Hugo Morgado <91498952+hmorgado24@users.noreply.github.com>
2021-09-27 23:54:16 +0000

    asd

--- lab2.py
+++ lab2.py
@@ -9,12 +9,9 @@ GPIO.setup(13, GPIO.OUT)
 GPIO.setup(25, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
 GPIO.setup(21, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
 
-pwm1 = GPIO.PWM(26, 100)
-# pwm3 = GPIO.PWM(13, 100)
-
 def blink():
   pwm2 = GPIO.PWM(19, 100)
-  pwm2.start(0)
+  pwm3 = GPIO.PWM(13, 100)
   try:
     while True:
       if GPIO.input(pwm2) == GPIO.HIGH:
@@ -24,14 +21,24 @@ def blink():
         for dc in range(0, 100, 1):
           pwm2.ChangeDutyCycle(dc)
           sleep(.01)
+      if GPIO.input(pwm3) == GPIO.HIGH:
+        for dc in range(100, 0, -1):
+          pwm3.ChangeDutyCycle(dc)
+          sleep(.01)
+        for dc in range(0, 100, 1):
+          pwm3.ChangeDutyCycle(dc)
+          sleep(.01)
   except KeyboardInterrupt:
     print('\nExiting')
   pwm2.stop()
+  pwm3.stop()
+  GPIO.cleanup()
 
 GPIO.add_event_detect(25, GPIO.RISING, callback=blink, bouncetime=200)
+GPIO.add_event_detect(21, GPIO.RISING, callback=blink, bouncetime=200)
 
 try:
-  pwm1.start(0)
+  pwm1 = GPIO.PWM(26, 100)
   while True:
     for dc in range(100, 0, -1):
       pwm1.ChangeDutyCycle(dc)
@@ -41,7 +48,6 @@ try:
       sleep(.01)
 except KeyboardInterrupt:
   print('\nExiting')
-
 pwm1.stop()
-# pwm3.stop()
+
 GPIO.cleanup()
