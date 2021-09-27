45d8aae56fafd2b035db05c3fb335d7d743d0fb1
Hugo Morgado <91498952+hmorgado24@users.noreply.github.com>
2021-09-27 23:35:35 +0000

    huh

--- lab2.py
+++ lab2.py
@@ -10,22 +10,23 @@ GPIO.setup(25, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
 #GPIO.setup(21, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
 
 pwm1 = GPIO.PWM(26, 1)
-# pwm2 = GPIO.PWM(19, 1)
+pwm2 = GPIO.PWM(19, 1)
 # pwm3 = GPIO.PWM(13, 1)
 
 def blink2(pin):
-  print("Rising edge detected on pin %d" % pin)
-  #pwm2.start(0)
-  # for dc in range(100, 0, -1):
-  #   pwm2.ChangeDutyCycle(dc)
-  #   sleep(.01)
-  # for dc in range(0, 100, 1):
-  #   pwm2.ChangeDutyCycle(dc)
-  #   sleep(.01)
+  pwm2.start(0)
+  while True:
+    for dc in range(100, 0, -1):
+      pwm2.ChangeDutyCycle(dc)
+      sleep(.01)
+    for dc in range(0, 100, 1):
+      pwm2.ChangeDutyCycle(dc)
+      sleep(.01)
 
 GPIO.add_event_detect(25, GPIO.RISING, callback=blink2, bouncetime=200)
 
-try: 
+try:
+  pwm1.start(0)
   while True:
     for dc in range(100, 0, -1):
       pwm1.ChangeDutyCycle(dc)
