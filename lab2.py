e782b739c517cff950b8e2c81d89c502630a3ad4
Hugo Morgado <91498952+hmorgado24@users.noreply.github.com>
2021-09-27 21:43:31 +0000

    sdf

--- lab2.py
+++ lab2.py
@@ -4,12 +4,14 @@ GPIO.setmode(GPIO.BCM)
 
 GPIO.setup(26, GPIO.OUT)
 pwm = GPIO.PWM(26, 1)
+
 try:
-  pwm.start(50)
+  pwm.start(0)
   while True:
-    pass
+    for dc in range(100, 0, -1):
+      sleep(.01)
 except KeyboardInterrupt:
-    print('\nExiting')
+  print('\nExiting')
 
 pwm.stop()
 GPIO.cleanup()
