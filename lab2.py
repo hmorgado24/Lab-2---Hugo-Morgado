118873f59528e3c93a6e5a018175102e79ae2cd8
Hugo Morgado <91498952+hmorgado24@users.noreply.github.com>
2021-09-27 22:02:19 +0000

    df

--- lab2.py
+++ lab2.py
@@ -4,12 +4,12 @@ from time import sleep
 GPIO.setmode(GPIO.BCM)
 
 GPIO.setup(26, GPIO.OUT)
-pwm = GPIO.PWM(26, 1)
+pwm = GPIO.PWM(26, 100)
 
 try:
   pwm.start(0)
   while True:
-    for dc in range(101):
+    for dc in range(100, 0, -1):
       pwm.ChangeDutyCycle(dc)
       sleep(.01)
 except KeyboardInterrupt:
