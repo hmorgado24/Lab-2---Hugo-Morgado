eade19c77376fdfd2cce9cfb51396eaef09bbf5d
Hugo Morgado <91498952+hmorgado24@users.noreply.github.com>
2021-09-27 23:15:25 +0000

    sdf

--- lab2.py
+++ lab2.py
@@ -12,7 +12,6 @@ GPIO.setup(20, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
 pwm1 = GPIO.PWM(26, 1)
 # pwm2 = GPIO.PWM(19, 1)
 # pwm3 = GPIO.PWM(13, 1)
-#
 
 def blink2(pin):
   print("Rising edge detected on pin %d" % pin)
@@ -24,17 +23,16 @@ def blink2(pin):
   #   pwm2.ChangeDutyCycle(dc)
   #   sleep(.01)
 
-try:  
-  # pwm1.start(0)
- # while True:
-  GPIO.add_event_detect(20, GPIO.RISING, callback=blink2, bouncetime=200)
-  for dc in range(100, 0, -1):
-    pwm1.ChangeDutyCycle(dc)
-    sleep(.01)
-  for dc in range(0, 100, 1):
-    pwm1.ChangeDutyCycle(dc)
-    sleep(.01)
+GPIO.add_event_detect(20, GPIO.RISING, callback=blink2, bouncetime=200)
 
+try: 
+  while True:
+    for dc in range(100, 0, -1):
+      pwm1.ChangeDutyCycle(dc)
+      sleep(.01)
+    for dc in range(0, 100, 1):
+      pwm1.ChangeDutyCycle(dc)
+      sleep(.01)
 except KeyboardInterrupt:
   print('\nExiting')
 
