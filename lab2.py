33c10f6c2e979a49f615c17447f3231df4952154
Hugo Morgado <91498952+hmorgado24@users.noreply.github.com>
2021-09-27 23:14:03 +0000

    asd

--- lab2.py
+++ lab2.py
@@ -3,13 +3,13 @@ from time import sleep
 
 GPIO.setmode(GPIO.BCM)
 
-# GPIO.setup(26, GPIO.OUT)
+GPIO.setup(26, GPIO.OUT)
 # GPIO.setup(19, GPIO.OUT)
 # GPIO.setup(13, GPIO.OUT)
 GPIO.setup(20, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
 #GPIO.setup(21, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
 
-# pwm1 = GPIO.PWM(26, 1)
+pwm1 = GPIO.PWM(26, 1)
 # pwm2 = GPIO.PWM(19, 1)
 # pwm3 = GPIO.PWM(13, 1)
 #
@@ -28,17 +28,17 @@ try:
   # pwm1.start(0)
  # while True:
   GPIO.add_event_detect(20, GPIO.RISING, callback=blink2, bouncetime=200)
-  #   for dc in range(100, 0, -1):
-  #     pwm1.ChangeDutyCycle(dc)
-  #     sleep(.01)
-  #   for dc in range(0, 100, 1):
-  #     pwm1.ChangeDutyCycle(dc)
-  #     sleep(.01)
+  for dc in range(100, 0, -1):
+    pwm1.ChangeDutyCycle(dc)
+    sleep(.01)
+  for dc in range(0, 100, 1):
+    pwm1.ChangeDutyCycle(dc)
+    sleep(.01)
 
 except KeyboardInterrupt:
   print('\nExiting')
 
-# pwm1.stop()
+pwm1.stop()
 # pwm2.stop()
 # pwm3.stop()
 GPIO.cleanup()
