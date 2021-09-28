13b086dd6db8dd95fb084e9beb65fdd6bada1fb0
Hugo Morgado <91498952+hmorgado24@users.noreply.github.com>
2021-09-28 00:32:31 +0000

    asd

--- lab2.py
+++ lab2.py
@@ -12,12 +12,11 @@ GPIO.setup(21, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
 pwm1 = GPIO.PWM(26, 100)
 pwm2 = GPIO.PWM(19, 100)
 pwm3 = GPIO.PWM(13, 100)
+pwm1.start(100)
 pwm2.start(100)
 pwm3.start(100)
 
 def blink(self):
-
-
   try:
     while True:
       if GPIO.input(25) == GPIO.HIGH:
@@ -44,7 +43,6 @@ GPIO.add_event_detect(25, GPIO.RISING, callback=blink, bouncetime=200)
 GPIO.add_event_detect(21, GPIO.RISING, callback=blink, bouncetime=200)
 
 try:
-  pwm1.start(0)
   while True:
     for dc in range(100, 0, -1):
       pwm1.ChangeDutyCycle(dc)
